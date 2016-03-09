5df402442a24aed14cd6473582e3c12c61260e52
Nopakorn Ganjanasinit <ssd@Siams-iMac.local>
2016-03-09 16:44:12 +0700

    adding firstSetting view class and method

---
+++ Youtube-Feed/FirstSettingTableViewController.h
@@ -0,0 +1,22 @@
+//
+//  FirstSettingTableViewController.h
+//  Youtube-Feed
+//
+//  Created by Siam System Deverlopment on 3/9/2559 BE.
+//  Copyright © 2559 guild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Youtube.h"
+
+@interface FirstSettingTableViewController : UITableViewController
+
+@property(strong, nonatomic) Youtube *youtube;
+@property (nonatomic, retain) NSMutableArray *genreList;
+@property (nonatomic, retain) NSMutableArray *genreSelected;
+
+
+@property(weak,nonatomic) IBOutlet UIButton  *submitButton;
+- (IBAction)submitButtonPressed:(id)sender;
+
+@end
---
+++ Youtube-Feed/FirstSettingTableViewController.m
@@ -0,0 +1,121 @@
+//
+//  FirstSettingTableViewController.m
+//  Youtube-Feed
+//
+//  Created by Siam System Deverlopment on 3/9/2559 BE.
+//  Copyright © 2559 guild. All rights reserved.
+//
+
+#import "FirstSettingTableViewController.h"
+#import "MainTabBarViewController.h"
+#import "SettingCustomCell.h"
+
+
+@interface FirstSettingTableViewController ()
+
+@end
+
+@implementation FirstSettingTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.genreSelected = [[NSMutableArray alloc] initWithCapacity:10];
+    [self createGerne];
+
+}
+
+- (void)createGerne
+{
+    self.genreList = [[NSMutableArray alloc] initWithObjects:@"Pop", @"Rock", @"Alternative Rock", @"Classical", @"Country", @"Dance", @"Folk", @"Indie", @"Jazz", @"Hip-hop", nil];
+    NSLog(@"count in create %lu",(unsigned long)[self.genreList count]);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return [self.genreList count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *tableIdentifier = @"SettingCustomCell";
+    //[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    SettingCustomCell *cell = [tableView dequeueReusableCellWithIdentifier:tableIdentifier];
+    if (cell == nil) {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"SettingCustomCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+    }
+    NSInteger row = indexPath.row;
+    if ([self checkmark:row]) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    cell.type.text = [self.genreList objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (BOOL) checkmark:(NSInteger )row
+{
+    NSString *item = [self.genreList objectAtIndex:row];
+    if(self.genreSelected != 0){
+        for (int i = 0; i < [self.genreSelected count]; i++) {
+            if([[self.genreSelected objectAtIndex:i] isEqualToString:item]){
+                return true;
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 53;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *item = [self.genreList objectAtIndex:indexPath.row];
+    if([self.genreSelected count] != 0 ){
+        for (int i=0; i<[self.genreSelected count]; i++) {
+            if([[self.genreSelected objectAtIndex:i] isEqualToString:item]){
+                [self.genreSelected removeObject:item];
+                break;
+            
+            }else if(i == [self.genreSelected count] - i){
+                [self.genreSelected addObject:item];
+                break;
+            }
+        }
+    }else{
+        [self.genreSelected addObject:[self.genreList objectAtIndex:indexPath.row]];
+    }
+    
+    [tableView reloadData];
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if(sender == self.submitButton){
+        NSLog(@"submitButton Action");
+        //[self.parentViewController.tabBarController setSelectedIndex:1];
+    }
+}
+
+
+
+@end
