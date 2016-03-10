6c1292329dbc103d224002554e96f26a2a2f35ff
Nopakorn Ganjanasinit <nopakorn.g@ssd.co.th>
2016-03-10 15:37:44 +0700

    adding popup playlist view controller

---
+++ Youtube-Feed/AddPlaylistPopUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddPlaylistPopUpViewController.h
+//  Youtube-Feed
+//
+//  Created by Siam System Deverlopment on 3/10/2559 BE.
+//  Copyright © 2559 guild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPlaylistPopUpViewController : UIViewController
+
+@end
---
+++ Youtube-Feed/AddPlaylistPopUpViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddPlaylistPopUpViewController.m
+//  Youtube-Feed
+//
+//  Created by Siam System Deverlopment on 3/10/2559 BE.
+//  Copyright © 2559 guild. All rights reserved.
+//
+
+#import "AddPlaylistPopUpViewController.h"
+
+@interface AddPlaylistPopUpViewController ()
+
+@end
+
+@implementation AddPlaylistPopUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
